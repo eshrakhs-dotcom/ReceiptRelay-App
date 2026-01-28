cade33c2ae0372898d5b339a84b30c44ed4305dd
Eshrak HS <eshrakhs@eshraks-air.uconnect.utah.edu>
2026-01-28 13:03:30 -0700

    Mark policy page dynamic to avoid prerender errors

--- app/settings/policy/page.tsx
+++ app/settings/policy/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { ensureUser, getPolicies, upsertPolicies } from '@/lib/data';
 
 export default async function PolicyPage() {
